d9ec542d2ef59b1a737456ea3b86efc69d70f49a
Cameron Smith <camerosmit@yahoo.com>
2018-03-20 16:58:46 -0700

    Updated the color scheme and added a new "btn-important" class.

--- index.php
+++ index.php
@@ -31,8 +31,8 @@
         <p class="paragraph">Hello, and welcome to my website! Here you can find a little bit about me and my work. Because I am studying German in my free time, I have also translated this entire website into German. As I am still learning, feel free to email me suggestions from the <a href="http://localhost/camerontsmith.tk/en/#">contact page</a>.</p>
         <p class="paragraph">Hallo und willkommen auf meiner Website! Hier finden Sie ein wenig über mich und meine Arbeit. Da ich in meiner Freizeit Deutsch studiere, habe ich diese komplette Website ins Deutsche übersetzt. Während ich noch lerne, kannst du mir gerne Vorschläge von der <a href="http://localhost/camerontsmith.tk/de/#">Kontaktseite</a> schicken.</p>
         <div class="center btn-group">
-          <a class="btn" href="http://localhost/camerontsmith.tk/en/">English</a>
-          <a class="btn" href="http://localhost/camerontsmith.tk/de/">Deutsch</a>
+          <a class="btn btn-important" href="http://localhost/camerontsmith.tk/en/">English</a>
+          <a class="btn btn-important" href="http://localhost/camerontsmith.tk/de/">Deutsch</a>
         </div>
       </main>
 
