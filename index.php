b9e214ee308634cc0824c71a3ad86592d5dbdbec
Cameron Smith <camerosmit@yahoo.com>
2018-03-20 21:10:33 -0700

    Finished the working color scheme and button prefabs

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
       <?php include './includes/header.inc.php'; ?>
 
       <main class="page">
-        <p class="paragraph">Hello, and welcome to my website! Here you can find a little bit about me and my work. Because I am studying German in my free time, I have also translated this entire website into German. As I am still learning, feel free to email me suggestions from the <a href="http://localhost/camerontsmith.tk/en/#">contact page</a>.</p>
-        <p class="paragraph">Hallo und willkommen auf meiner Website! Hier finden Sie ein wenig über mich und meine Arbeit. Da ich in meiner Freizeit Deutsch studiere, habe ich diese komplette Website ins Deutsche übersetzt. Während ich noch lerne, kannst du mir gerne Vorschläge von der <a href="http://localhost/camerontsmith.tk/de/#">Kontaktseite</a> schicken.</p>
+        <p class="paragraph">Hello, and welcome to my website! Here you can find a little bit about me and my work. Because I am studying German in my free time, I have also translated this entire website into German. As I am still learning, feel free to email me suggestions from the <a href="http://localhost/camerontsmith.tk/en/contact/">contact page</a>.</p>
+        <p class="paragraph">Hallo und willkommen auf meiner Website! Hier finden Sie ein wenig über mich und meine Arbeit. Da ich in meiner Freizeit Deutsch studiere, habe ich diese komplette Website ins Deutsche übersetzt. Während ich noch lerne, kannst du mir gerne Vorschläge von der <a href="http://localhost/camerontsmith.tk/de/contact/">Kontaktseite</a> schicken.</p>
         <div class="center btn-group">
           <a class="btn btn-important" href="http://localhost/camerontsmith.tk/en/">English</a>
           <a class="btn btn-important" href="http://localhost/camerontsmith.tk/de/">Deutsch</a>
