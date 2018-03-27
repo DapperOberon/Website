eab4a29ac91c468274c4fdea273ee11c83d70b6a
Cameron Smith <camerosmit@yahoo.com>
2018-03-26 17:01:49 -0700

    Updated the about page

--- de/about.php
+++ de/about.php
@@ -9,9 +9,15 @@
       <?php include 'http://localhost/camerontsmith.tk/de/includes/header.inc.php'; ?>
 
       <main class="page">
-        <p>
-          About in German.
-        </p>
+        <figure class="page-figure-left">
+          <img src="http://localhost/camerontsmith.tk/images/profile.jpg" alt="a photo of me"/>
+        </figure>
+
+        <p>Hi, ich bin Cameron Smith, ein Student, der Front-End Web Development (FEWD) und Game Design studiert. Ich liebe es, Websites und Spiele zu gestalten, Fotos zu machen und Klavier zu spielen.</p>
+
+        <p>Ich bin in Oregon, USA, und arbeite derzeit an meinem Informationstechnologie an der Liberty University Online. Ich habe Web Design in meiner Freizeit in den letzten acht Jahren und Game Design in den letzten zwei Jahren studiert. Ich plante ursprünglich, Grafikdesign zu studieren, fand aber Webdesign und Spieledesign interessanter und bin seitdem süchtig danach.</p>
+
+        <p>Die Seite befindet sich derzeit im Aufbau, bitte haben Sie etwas Geduld.</p>
       </main>
 
       <?php include 'http://localhost/camerontsmith.tk/de/includes/footer.inc.php'; ?>
--- en/about.php
+++ en/about.php
@@ -8,9 +8,15 @@
       <?php include 'http://localhost/camerontsmith.tk/en/includes/header.inc.php'; ?>
 
       <main class="page">
-        <p>
-          This is the about me page.
-        </p>
+        <figure class="page-figure-left">
+          <img src="http://localhost/camerontsmith.tk/images/profile.jpg" alt="a photo of me"/>
+        </figure>
+
+        <p>Hi, I'm Cameron Smith a student studying Front-End Web Development (FEWD) and Game Design. I love designing websites and games as well as taking photos and playing piano.</p>
+
+        <p>I'm based in Oregon, USA and currently working on my Information Technology degree at Liberty University Online. I have been studing web design in my free time for the past eight years and game design for the past two years. I orignially planned to study graphic design but found web design and game design more interesting and have been hooked ever since.</p>
+
+        <p>The site is currently under construction, so please be patient.</p>
       </main>
 
       <?php include 'http://localhost/camerontsmith.tk/en/includes/footer.inc.php'; ?>
